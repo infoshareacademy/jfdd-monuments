b4b75cc1c6821decc53bb4e0804a8338d797c67a
Krzysztof <kgolat@gmail.com>
2016-01-05 22:34:52 +0100

    EasterEgg

--- js/easteregg.js
+++ js/easteregg.js
@@ -1,7 +1,35 @@
-//function myFunction() {
-//    document.getElementsByClassName('easter')[0].onclick() = "yellow";
+$(document).ready(function() {
+    $('.easter__close').click(function() {
+        $('.easter').fadeOut(500);
+        $('.overlay').fadeOut(500);
+        $('.easter span:not(.easter__counters__highscore) em').text('0');
+    });
 
-function myEaster() {
-    document.getElementById('demo').style.color = "red";
-    document.getElementById('easter-egg-container').style.display = "block";
-}
+    $('.wywolanie__easter').click(function() {
+        $('.easter').fadeIn(500);
+        $('.overlay').fadeIn(500);
+
+        var mapWidth = $('.easter__map').width();
+        console.log(mapWidth);
+        var mapHeight = $('.easter__map').height();
+        console.log(mapHeight);
+        var routeLimit = mapHeight + mapWidth;
+        console.log(routeLimit);
+        routeLimit = Math.round(routeLimit);
+        $('.easter__counters__limit em').text(routeLimit);
+    });
+
+    $( window ).resize(function() {
+        if ($('.easter').is(":visible")) {
+            var mapWidth = $('.easter__map').width();
+            console.log(mapWidth);
+            var mapHeight = $('.easter__map').height();
+            console.log(mapHeight);
+            var routeLimit = mapHeight + mapWidth;
+            console.log(routeLimit);
+            routeLimit = Math.round(routeLimit);
+            $('.easter__counters__limit em').text(routeLimit);
+        }
+    });
+
+});
