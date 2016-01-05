8aeec6bdfb7666bc50e1d65848e41d6e75c7772f
Krzysztof <kgolat@gmail.com>
2016-01-05 17:01:00 +0100

    Wywołanie EasterEgg

---
+++ js/easteregg.js
@@ -0,0 +1,6 @@
+//function myFunction() {
+//    document.getElementsByClassName('easter')[0].onclick() = "yellow";
+
+function myEaster() {
+    document.getElementsByName('demo').style.color = "red";
+}
