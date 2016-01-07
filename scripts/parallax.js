5ed7aa14d92eb3e855cb9cdb194bd7350f3fe685
GGoo <piekarska@live.com>
2016-01-07 21:48:34 +0100

    parallax cdn

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -2,36 +2,27 @@
  * Created by gosia on 07.01.16.
  */
 
-//
-//$(document).ready(function(){
-//
-//    var poprzedniScrollOkna = 0;
-//    var elementZTlem = $('.header');
-//
-//    $(window).scroll(
-//        function() {
-//            var aktualnyScrollOkna = $(window).scrollTop();
-//            var aktualnyYTla = elementZTlem.css('background-position-y').split('px')[0]*1;
-//            var wektorZmianyTla = 30 ;
-//
-//            var kierunek = (poprzedniScrollOkna > aktualnyScrollOkna) ? 1 : -1;
-//
-//
-//            elementZTlem.css('background-position-y', aktualnyYTla - wektorZmianyTla*kierunek);
-//
-//
-//            poprzedniScrollOkna = aktualnyScrollOkna;
-//        }
-//    )
-//
-//});
-
 
 $(document).ready(function(){
-    $(window).on('scroll', function() {
-        $('.header').css('background-position-y', $(window).scrollTop() * -.3);
-    });
+
+    var poprzedniScrollOkna = 0;
+    var elementZTlem = $('.header');
+
+    $(window).scroll(
+        function() {
+            var aktualnyScrollOkna = $(window).scrollTop();
+            var aktualnyYTla = elementZTlem.css('background-position-y').split('px')[0]*1;
+            var wektorZmianyTla = 30 ;
+
+            var kierunek = (poprzedniScrollOkna > aktualnyScrollOkna) ? 1 : -1;
 
 
+            elementZTlem.css('background-position-y', aktualnyYTla - wektorZmianyTla*kierunek);
+
+
+            poprzedniScrollOkna = aktualnyScrollOkna;
+        }
+    )
 
 });
+
