551c745a9b4eba6b4e0ce8baa7f823e0ffb33808
GGoo <piekarska@live.com>
2016-01-08 16:48:01 +0100

    ready parallax

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -4,16 +4,19 @@
 
 
 $(document).ready(function(){
-    var elementZTlem = $('.header');
-    $(window).on("load scroll",
+    var elementZTlem = $('.header');//zapisanie w zmiennej elementu z domku
+    $(window).on("load scroll",//dodanie listenera na event load i scroll - na jaki event czyli ON
         function() {
-            var aktualnyScrollOkna = $(window).scrollTop();
-            var wektorZmianyTla = aktualnyScrollOkna *0.6 ;
-
-            elementZTlem.css('background-position-y', wektorZmianyTla - 200);
+            var aktualnyScrollOkna = $(window).scrollTop();//zwraca przesuniecie okna po scrollu
+            //
+            var wektorZmianyTla = aktualnyScrollOkna *0.8 ;//wyliczanie wektora zmiany
+//
+            elementZTlem.css('background-position-y', wektorZmianyTla - 300);// 200 to poczatkowe przesuniecie tla voila,
+            //zwraca wartosc w/w linijka
 
         }
     )
 
 });
+
 
