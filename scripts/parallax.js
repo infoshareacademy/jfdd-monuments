0ed1633b4cdef85e3d7e54bf3aebda492a3255ca
GGoo <piekarska@live.com>
2016-01-08 16:08:39 +0100

    ready parallax

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -4,23 +4,14 @@
 
 
 $(document).ready(function(){
-
-    var poprzedniScrollOkna = 0;
     var elementZTlem = $('.header');
-
-    $(window).scroll(
+    $(window).on("load scroll",
         function() {
             var aktualnyScrollOkna = $(window).scrollTop();
-            var aktualnyYTla = elementZTlem.css('background-position-y').split('px')[0]*1;
-            var wektorZmianyTla = 30 ;
+            var wektorZmianyTla = aktualnyScrollOkna *0.6 ;
 
-            var kierunek = (poprzedniScrollOkna > aktualnyScrollOkna) ? 1 : -1;
+            elementZTlem.css('background-position-y', wektorZmianyTla - 200);
 
-
-            elementZTlem.css('background-position-y', aktualnyYTla - wektorZmianyTla*kierunek);
-
-
-            poprzedniScrollOkna = aktualnyScrollOkna;
         }
     )
 
