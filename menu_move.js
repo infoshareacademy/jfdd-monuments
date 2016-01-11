d95ffffdddcef735a5f459ae757f0da5281b0ab3
lukasz184 <deput.lukasz@gmail.com>
2016-01-11 17:01:20 +0100

    zmiany3 w js menu

--- menu_move.js
+++ menu_move.js
@@ -1,12 +1,14 @@
 $( document ).ready( function() {
     $(window).scroll(function) {
-        var doc-top = $(window).scrollTop();
-        var div-glowna = $('#nasz-header').offset().top;
-        if (doc_top >= div_glowna) {
-            $('.menu_div_header').removeClass('');
-            $('.hover_orange').addClass('');
-        } else if (doc_top >= $("#features_id")
-    }
-}
+        var doc_top = $(window).scrollTop() + 10;
+        var div_glowna = $('#nasz-header').offset().top;
+        if (doc_top >= div_glowna && doc_top <= ) {
+            $('menu_div_header').addClass('hover_orange');
+        } else
+            $('menu_div_header').removeClass('hover_orange');
+        }
+        if ()
+    });
+
 
 
