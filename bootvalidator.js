2d2dd9f56d585dfd124f418734e31855c5d3f054
GGoo <piekarska@live.com>
2016-01-12 20:06:08 +0100

    preparation for the boot validator

---
+++ bootvalidator.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Gosia on 12/01/16.
+ */
+
+
+//$(document).ready(function)
+//{
+//
+//
+//    var keyDown = $(document).onkeydown(function)
+//    {
+////$('.contact_form').validate
+//    })
+//
+//    var mouseClick = $(docment).onmousedown(function () {
+//
+//
+//    })
+//
+//    var
+//
