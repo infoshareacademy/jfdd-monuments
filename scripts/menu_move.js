5584286f6d0ce60a3d7ddb87f16da17fc5978b3e
lukasz184 <deput.lukasz@gmail.com>
2016-01-17 23:53:36 +0100

    menu reagujące na położenie działa

--- scripts/menu_move.js
+++ scripts/menu_move.js
@@ -7,10 +7,9 @@ $(document).ready(function () {
         var doc_top = $(window).scrollTop() + 10,
             menu_glowna = $('#nasz_header').offset().top,
             menu_feat = $('#features_id').offset().top,
-        //menu_add_feat = $('feat_add_id').offset().top,
+            menu_add_feat = $('#feat_add_id').offset().top,
             menu_contact = $('#contact_id').offset().top;
 
-        console.log(menu_glowna, menu_feat, menu_contact);
 
         if (doc_top < menu_feat) {
             $('.menu_div_header').addClass('hover_js');
@@ -19,17 +18,17 @@ $(document).ready(function () {
         }
 
 
-        if (doc_top >= menu_feat && doc_top <= menu_contact) /*menu_add_feat*/{
+        if (doc_top >= menu_feat && doc_top <= menu_add_feat) {
             $('.menu_div_feat').addClass('hover_js');
         } else {
             $('.menu_div_feat').removeClass('hover_js');
         }
 
-        //if (doc_top >= menu_add_feat && doc_top <= menu_contact) {
-        //    $('.menu_div_add_feat').addClass('hover_js');
-        //} else {
-        //    $('.menu_div_add_feat').removeClass('hover_js');
-        //}
+        if (doc_top >= menu_add_feat && doc_top <= menu_contact) {
+            $('.menu_div_add_feat').addClass('hover_js');
+        } else {
+            $('.menu_div_add_feat').removeClass('hover_js');
+        }
 
         if (
             doc_top >= menu_contact ||
