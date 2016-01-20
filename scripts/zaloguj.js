8da4e4d558f88d172f6e2227ddb22eef8c11cf13
lukasz184 <deput.lukasz@gmail.com>
2016-01-20 16:37:39 +0100

    pole zaloguj podpięte pod button i lekko wystylizowane

--- scripts/zaloguj.js
+++ scripts/zaloguj.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
+
+    // wywołanie zaloguj
+    $('.zaloguj__wywolanie').click(function () {
+        $('.zaloguj').fadeIn(500);
+        $('.zaloguj__background').fadeIn(500);
+
+    })
+    // zamkniecie zaloguj
     $('.zaloguj__close').click(function () {
         $('.zaloguj').fadeOut(500);
         $('.zaloguj__background').fadeOut(500);
 
 
     });
-
-
-    //wywołanie zaloguj
-    $('.zaloguj__wywolanie').click(function () {
-        $('.zaloguj').fadeIn(500);
-        $('.zaloguj__background').fadeIn(500);
-
-    })
-
 });
+
+
+
