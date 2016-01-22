3548cdbb672304cd4c43746d3ac47d23ad072405
Krzysztof <kgolat@gmail.com>
2016-01-22 16:46:07 +0100

    Dodanie logowania po Google Plus

--- scripts/zaloguj.js
+++ scripts/zaloguj.js
@@ -15,6 +15,31 @@ $(document).ready(function() {
     });
 });
 
+
+
+// ZALOGUJ PO GOOGLE PLUS
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+
+    debugger;
+
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+
+    window.location = 'http://test.app.monuments.jfdd.infoshareaca.nazwa.pl';
+};
+
+
+
+// ZALOGUJ PO FACEBOOKU
 //
 //function statusChangeCallback(response) {
 //    console.log('statusChangeCallback');
